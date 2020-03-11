dd65f92b86d881ac681093685210f6680a5f75ec
Harrison Levin <harrison483@gmail.com>
2020-03-11 14:14:16 -0600

    added expected return to asset class model

--- frontier/models.py
+++ frontier/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 
 class AssetClass(models.Model):
     name = models.CharField(max_length=100)
+    expected_return = models.FloatField()
     date_created = models.DateTimeField(default=timezone.now)
 
     def __self__(self):
