80ab9eb46f687528bf7712e5ecd38f13c9ea13af
Harrison Levin <harrison483@gmail.com>
2020-03-11 15:56:06 -0600

    template, pick asset class page

--- frontier/views.py
+++ frontier/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import AssetClass
 
 def index(request):
-    return render(request, 'frontier/index.html', context=None)
+    context = {
+        'asset_classes': AssetClass.objects.all()
+    }
+    return render(request, 'frontier/index.html', context)
