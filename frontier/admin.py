6567e02d92427b82fdf8a445d400819d077367c5
Harrison Levin <harrison483@gmail.com>
2020-03-11 13:19:43 -0600

    data and asset class model

--- frontier/admin.py
+++ frontier/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import AssetClass
+
+admin.site.register(AssetClass)
 
 # Register your models here.
--- frontier/models.py
+++ frontier/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class AssetClass(models.Model):
+    name = models.CharField(max_length=100)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __self__(self):
+        return self.name
